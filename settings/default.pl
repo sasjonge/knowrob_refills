6ed237f9ae12baa98a862fde3a7a30d5e0714dee
ARRINA user on robot <ai-arrina@cs.uni-bremen.de>
2020-11-10 16:59:57 +0100

    added setttings folder

---
+++ settings/default.pl
@@ -0,0 +1,21 @@
+
+%	Plugins
+setting(knowrob:plugins,
+	[ 'ros/tf/tf_plugin',
+	  'ros/marker/marker_plugin'
+	]).
+
+%	Triple DB
+setting(tripledb:path, 'db/mongo/tripledb/plugin').
+setting(tripledb:drop_graphs, []). % erase "user" graph on start-up
+
+%	Mongo DB
+setting(mng_client:db_name, roslog).
+
+% Flag for read only mongo databases
+setting(mng_client:read_only, false).
+
+%	Remote Mongo Setting
+setting(mng_client:collection_prefix, '').
+
+
